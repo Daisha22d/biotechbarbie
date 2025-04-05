f37beab6af6330f6b26154db6c6019dc41be6318
Daisha <daishadeniz@gmail.com>
2025-04-04 23:44:14 -0400

    fixed commas

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,19 +12,19 @@ export default function About() {
           <div>
             <h1 className="text-4xl font-bold text-pink-400">About Me</h1>
             <p className="text-lg text-gray-800 mt-4 max-w-2xl">
-              As a data scientist and lifelong learner, I'm currently completing my
-              Master’s in Data Intelligence and Machine Learning at Mercer University.
-              I'm passionate about transforming complex data into visually compelling
+              As a data scientist and lifelong learner, I am currently completing my
+              Masters in Data Intelligence and Machine Learning at Mercer University.
+              I am passionate about transforming complex data into visually compelling
               stories, and I thrive on tackling new challenges.
             </p>
             <p className="text-lg text-gray-800 mt-4 max-w-2xl">
-              When I'm not immersed in code or charts, you'll find me at the gym,
+              Besides immersing myself in code or charts, you will find me at the gym,
               enjoying a good book at a coffee shop, or diving into a new hobby for
               the sheer joy of it.
             </p>
             <p className="text-lg text-gray-800 mt-4 max-w-2xl">
               Fueled by a genuine curiosity and a drive to understand the "why" behind
-              everything, I'm constantly exploring—whether it's in data, in life, or
+              everything, I am constantly exploring—whether it is in data, in life, or
               anywhere in between.
             </p>
           </div>
